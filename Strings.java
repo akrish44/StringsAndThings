1be23ea12b49869be6ddda449d11fc029e2b325f
Susmita <akrish44@aol.com>
2013-09-15 16:49:39 -0600

    Created is Dupledrome

--- Strings.java
+++ Strings.java
@@ -1,22 +1,38 @@
 public class Strings {
 	
 	public static void main(String[] args) {
-		char[] abecArray = {'d', 'i', 'm', 'p', 's', 'y'};
+		// char[] abecArray = {'d', 'i', 'm', 'p', 's', 'y'};
+		char[] dupleArray = {'h', 'e', 'l', 'l', 'o'};
+		
 
+		// System.out.println("This string is " + isAbecedarian(abecArray));
+		System.out.println("The string is " + isDupledrome(dupleArray));
 
-		System.out.println("This string is " + isAbecedarian(abecArray));
 
 	}
-	public static boolean isAbecedarian(char[] localArray) {
+	// public static boolean isAbecedarian(char[] localArray) {
 	
+	// 	int size = localArray.length;
+	// 	for(int i=0; i < (size-1); i++) {
+	// 		if (localArray[i] >= localArray[i+1]) {
+	// 			return false;
+	// 		}
+	// 	} 
+	// 	return true;
+	// }
+
+	public static boolean isDupledrome(char[] localArray) {
+
 		int size = localArray.length;
 		for(int i=0; i < (size-1); i++) {
-			if (localArray[i] >= localArray[i+1]) {
+			if (localArray[i] != localArray[i+1]) {
 				return false;
 			}
-		} 
+			return true;
+		}
 		return true;
 	}
+
 
 
 }
